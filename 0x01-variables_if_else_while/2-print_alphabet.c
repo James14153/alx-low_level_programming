e57ed04a5e45f16d0f3371a63bbfb2e77a7ccf86
James14153 <Jamesvincent903@gmail.com>
2022-09-12 02:55:48 -0700

    ifs

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,3 +1,18 @@
 #include <stdio.h>
 /**
- * main-program run
+ * main-program run from here
+ * 
+ * return zero if no error and non-zero if error
+ **/
+int main(void)
+{
+	int alphabet;
+	alphabet = 'a';
+	while (alphabet <= 'z')
+	{
+		putchar(alphabet <= 'z');
+		alphabet++;
+	}
+	putchar("\n");
+	return (0);
+}
