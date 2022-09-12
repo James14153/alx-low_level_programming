d0bb23fd07cc45ffcbb08344300bdabbbaf6d84c
James14153 <Jamesvincent903@gmail.com>
2022-09-12 14:25:57 -0700

    ifs

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,17 +1,22 @@
+#include <stdio.h>
 /**
- * display abse 16 numbers in lowercase
- * 
- *return 0
+ *main -  prints base 16 numbers in lowercase
+ *
+ *Return: Always 0.
  **/
 int main(void)
 {
-	int var1;
-	char var2;
-	for (var1 = 0; var1 < 10; var1++)
-		putchar((var1 % 10) + '0');
-	for (var2 = 'a'; var2 <= 'f'; var2++)
+	int num;
+	char letter;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	for (letter = 'a'; letter <= 'f'; letter++)
 		putchar(letter);
+
 	putchar('\n');
+
 	return (0);
 }
 
