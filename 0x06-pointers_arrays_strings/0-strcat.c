e1ef0a211f946eb7d4e5c792d9dd8b10eb2200bb
James14153 <Jamesvincent903@gmail.com>
2022-09-21 22:09:02 -0700

    initial commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,1 +1,23 @@
 #include "main.h"
+
+/**
+ * _strcat - a function that concatenates two strings
+ * @dest: copy to
+ * @src: copy from
+ * Return: two contact strings
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int n;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	}
+
+	for (n = 0; (dest[i + n] = *src++) != '\0'; n++)
+	{
+	}
+
+	return (dest);
+}
