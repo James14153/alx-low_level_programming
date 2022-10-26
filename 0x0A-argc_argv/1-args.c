771a8f6494dd93ef817412d82dab758294550e27
James14153 <Jamesvincent903@gmail.com>
2022-10-26 05:13:45 -0700

    corrections

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,22 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - prints the number of arguments passed to it
+ * main - prints the name number of arguments passed to it
+ * @argc: number of arguments passed to the function
+ * @argv: argument vector of pointers to strings
  *
- * @argc: argument count
- * @argv: argument vector
- *
- * Return: 0
+ * Return: always 0
  */
-
-int main(int argc, char argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-	int d;
-	(void)argv;
-
-	for (d = 0; d <= argc; d++)
-	{}
-	printf("%d\n", d - 2);
-
+	printf("%d\n", argc - 1);
 	return (0);
 }
--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
 
 /**
- * main - prints the number of arguments passed to it
+ * main - prints all arguments it recieves
+ * @argc: number of arguments passed to the function
+ * @argv: argument vector of pointers to strings
  *
- * @argc: argument count
- * @argv: argument vector
- *
- * Return: 0
+ * Return: always 0
  */
-
-int main(int argc, char argv[])
+int main(int argc, char *argv[])
 {
-	int d;
+	int i;
 
-	for (d = 0; d < argc; d++)
-		printf("%d\n", argv[d]);
-
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
 	return (0);
 }
