e686430d957754325bc2ef197269f15dddfacc20
James14153 <Jamesvincent903@gmail.com>
2022-11-04 12:23:58 -0700

    corrections

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,41 +1,38 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
-
-/* betty style doc for function main goes there */
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always success
  */
 int main(void)
 {
-	int i;
-	int j;
 
-	i = 48;
-	j = 49;
-	while  ((i < 57) && (j < 58))
-	{
-		putchar(i);
-		putchar(j);
-		if ((i == 56) && (j == 57))
-		{
-			putchar('\n');
-			i++;
-			j++;
-		}
-		else
-		{
-			putchar(44);
-			putchar(32);
-			if (j < 57)
-			{
-				i++;
-				j = 1 + i;
-			}
-		}
-	}
+int digit1, digit2;
 
-	return (0);
+for (digit1 = 0; digit1 < 9; digit1++)
+{
+
+for (digit2 = digit1 + 1; digit2 < 10; digit2++)
+
+{
+
+putchar((digit1 % 10) + '0');
+putchar((digit2 % 10) + '0');
+
+if (digit1 == 8 && digit2 == 9)
+
+continue;
+
+putchar(',');
+putchar(' ');
+
+}
+
+}
+
+putchar('\n');
+
+return (0);
 }
