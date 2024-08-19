607f43f9103dacaa338c0d433365faadee843941
James14153 <jamesvincent903@gmail.com>
2024-08-19 08:38:09 -0700

    task 3 correction

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,27 +9,26 @@
  */
 int main(int argc, char *argv[])
 {
-    int num1, num2;
-    int (*operation)(int, int);
+	int num1, num2;
+	int (*operation)(int, int);
 
-    if (argc != 4)
-    {
-        printf("Error\n");
-        return (98);
-    }
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
 
-    operation = get_op_func(argv[2]);
+	operation = get_op_func(argv[2]);
 
-    if (!operation || argv[2][1] != '\0')
-    {
-        printf("Error\n");
-        return (99);
-    }
+	if (!operation || argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		return (99);
+	}
 
-    num1 = atoi(argv[1]);
-    num2 = atoi(argv[3]);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
 
-    printf("%d\n", operation(num1, num2));
-
-    return (0);
+	printf("%d\n", operation(num1, num2)
+	return (0);
 }
