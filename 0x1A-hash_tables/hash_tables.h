dd63e10308809013b65e85ce64aca3b935564329
james14153 <jamesvincent903@gmail.com>
2025-08-01 00:17:10 -0700

    creating a hash table

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -7,8 +7,9 @@
 
 /* data structures */
 
-/*
+/**
  * struct hash_node_s - node of the hash tables
+ *
  * @key: the key, string
  * the key is unique in the hash table
  * @value: the value corresponding to a key
@@ -20,10 +21,11 @@ typedef struct hash_node_s
 	char *key;
 	char *value;
 	struct hash_node_s *next;
-}hash_node_t;
+} hash_node_t;
 
-/*
+/**
  * struct hash_table_s - structure of hash table
+ *
  * @size: the size of array
  * @array: an array of @size
  * each cell of this array is a pointer to the first node of a linked list
@@ -34,7 +36,7 @@ typedef struct hash_table_s
 {
 	unsigned long int size;
 	hash_node_t **array;
-}hash_table_t;
+} hash_table_t;
 
 /* function prototypes  */
 hash_table_t *hash_table_create(unsigned long int size);
